ca6e9b10c45e6ef850648529d6e1c061f77da039
MOON <moonkihyun95@gmail.com>
2024-03-20 21:16:31 +0900

    git init

---
+++ src/main/java/com/moon/lifestream/LifestreamApplication.java
@@ -0,0 +1,13 @@
+package com.moon.lifestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifestreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/lifestream/LifestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.lifestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifestreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
