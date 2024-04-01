3f4a0a3a28cb71db019730ead44d93907723c44f
MOON <moonkihyun95@gmail.com>
2024-04-01 21:57:55 +0900

    Feat : 테스트 부분 삭제

--- src/main/java/com/moon/lifestream/domain/member/controller/MemberController.java
+++ src/main/java/com/moon/lifestream/domain/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.moon.lifestream.domain.member.controller;
 import com.moon.lifestream.domain.member.dto.MemberSignupRequest;
 import com.moon.lifestream.domain.member.service.MemberService;
 import com.moon.lifestream.global.response.ApiResponse;
-import io.jsonwebtoken.lang.Strings;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.*;
 
 @RequiredArgsConstructor
 
@@ -31,33 +28,4 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.CREATED.value(), "Sign up completed"));
     }
 
-    public static void main(String[] args) {
-
-        List<String> stringsA = new LinkedList<>();
-        List<String> stringsB = new ArrayList<>();
-
-        for (int i = 0; i < 10000; i ++) {
-            stringsA.add(String.valueOf(i));
-            stringsB.add(String.valueOf(i));
-        }
-
-        long startTime;
-        long endTime;
-
-        // LinkedList 시간
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i ++) {
-            stringsA.add(0, String.valueOf(i));
-        }
-        endTime = System.currentTimeMillis();
-        System.out.printf("%-17s  %8d ms \n", "LinkedList 걸린 시간", (endTime - startTime));
-
-        // ArrayList 시간
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i ++) {
-            stringsB.add(0, String.valueOf(i));
-        }
-        endTime = System.currentTimeMillis();
-        System.out.printf("%-17s  %8d ms \n", "ArrayList 걸린 시간", (endTime - startTime));
-    }
 }
